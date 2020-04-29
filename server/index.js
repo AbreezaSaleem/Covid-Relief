e1bec32c586a714c5cf5da160880a4e579921545
Ali Ahsan <aliahsan07@outlook.com>
2020-04-29 07:48:46 -0500

    reconstructed message to user

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
-const path = require('path')
-const express = require('express');
-const bodyParser = require('body-parser');
-const pino = require('express-pino-logger')();
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const pino = require("express-pino-logger")();
 
-require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -11,30 +11,50 @@ app.use(bodyParser.json());
 // app.use(pino);
 const port = 8000;
 
-const data = require('./content/charities')
+const data = require("./content/charities");
 
 let serviceSID;
-const client = require('twilio')(
+const client = require("twilio")(
   process.env.TWILIO_ACCOUNT_SID,
   process.env.TWILIO_AUTH_TOKEN
 );
 
-app.post('/message', (req, res) => {
-  res.header('Content-Type', 'application/json');
-  const charityDetails = data.filter((charity) => charity.name === req.body.name);
+function constuctMessage(content) {
+  content = content[0];
+  var webpage = content.webpage || "Not Provided";
+  var contactNumber = content.contact || "Not Provided";
+  var easyPaisa = content.easyPaisa || "Not Provided";
+  var bankDetails = content.bankDetails[0];
+  var accountTitle = bankDetails.accountTitle || "Not Provided";
+  var bankName = bankDetails.bankName || "Not Provided";
+  var accountNumber = bankDetails.accountNumber || "Not Provided";
+  var iban = bankDetails.iban || "Not Provided";
+  var message = `Charity name: ${content.name}\nwebsite: ${webpage}\nContact Number: ${contactNumber}\nEasyPaisa: ${easyPaisa}\nBank Details are as follows\nAccount Title: ${accountTitle}\nBank Name: ${bankName}\nAccount Number: ${accountNumber}\nIBAN: ${iban}\nRamadan Mubarak!`;
+  return message;
+}
+
+app.post("/message", (req, res) => {
+  res.header("Content-Type", "application/json");
+  const charityDetails = data.filter(
+    (charity) => charity.name === req.body.name
+  );
+  var msg = constuctMessage(charityDetails);
   client.messages
-  .create({
-    from: process.env.TWILIO_PHONE_NUMBER,
-    to: req.body.number,
-    body: JSON.stringify(charityDetails)
-  })
-  .then(() => {
-    res.send(JSON.stringify({ success: true }));
-  })
-  .catch(err => {
-    console.log(err);
-    res.send(JSON.stringify({ success: false }));
-  });
+    .create({
+      from: process.env.TWILIO_PHONE_NUMBER,
+      to: req.body.number,
+      body: msg,
+    })
+    .then(() => {
+      console.log("Message sent succesfully");
+      res.send(JSON.stringify({ success: true }));
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send(JSON.stringify({ success: false }));
+    });
 });
 
-app.listen(port, () => console.log(console.log(`🚀 Server ready at localhost:${port}`)))
+app.listen(port, () =>
+  console.log(console.log(`🚀 Server ready at localhost:${port}`))
+);
